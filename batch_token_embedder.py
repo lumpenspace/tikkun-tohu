733fd07b9e14bc73da8d6541d3f3ee00af731258
lumpenspace <125578322+lumpenspace@users.noreply.github.com>
2024-01-19 00:30:42 +0100

    Basic functionality

---
+++ batch_token_embedder.py
@@ -0,0 +1,19 @@
+import openai
+import tiktoken
+
+def get_embeddings(token_batch, model):
+    response = openai.Embedding.create(input=token_batch, model=model)
+    return response['data']
+
+def process_token_batch(start_token, end_token, model, encoding="cl100k_base"):
+    token_batch = [[i] for i in range(start_token, end_token)]
+    embeddings_data = get_embeddings(token_batch, model)
+    token_embeddings = []
+
+    for idx, data in enumerate(embeddings_data):
+        token_id = start_token + idx
+        string_value = tiktoken.decode([token_id], encoding=encoding)
+        embedding = data['embedding']
+        token_embeddings.append([token_id, string_value, embedding])
+    
+    return token_embeddings
---
+++ openai.py
@@ -0,0 +1,7 @@
+from openai import OpenAI, OpenAIError
+from dotenv import load_dotenv
+from os import environ as os_env
+load_dotenv()
+
+oai_client = OpenAI(api_key=os_env.get("OPENAI_API_KEY"))
+
